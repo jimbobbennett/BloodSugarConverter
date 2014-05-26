67e9279a4e38e309caa3d329e3bc842f8a4158ba
Jim Bennett <jimbobbennett@mac.com>
2014-05-26 12:44:22 -0300

    Working version

---
+++ BloodSugarConverter/README.md
@@ -0,0 +1,8 @@
+HelloWorldCordova
+=================
+
+Since the release of [Cordova tools for Visual Studio](http://msdn.microsoft.com/en-us/vstudio/dn722381) I've decided to have a play to see what I can create.
+
+I'll be blogging about the process on my [Ghost](http://ghost.org) blog:
+
+http://jimbobbennett.ghost.org
---
+++ BloodSugarConverter/js/controllers.js
@@ -0,0 +1,17 @@
+﻿var bloodSugarConversionApp = angular.module('BloodSugarConversionApp', []);
+
+bloodSugarConversionApp.controller('BloodSugarConversionCtrl', function ($scope)
+{
+    $scope.usMeasurement = 0;
+    $scope.internationalMeasurement = 0;
+
+    $scope.changeUSMeasurement = function()
+    {
+        $scope.internationalMeasurement = Math.round(($scope.usMeasurement / 18) * 10) / 10;
+    };
+
+    $scope.changeInternationalMeasurement = function()
+    {
+        $scope.usMeasurement = Math.round(($scope.internationalMeasurement * 18) * 10) / 10;
+    };
+});
